35929f8266e7665e87ecc045f079773b17642303
f.nabizadeh <fatemenabizade@hotmail.com>
2023-12-11 16:50:49 +0330

    test detail show in h2 tag

--- src/app/components/post-detail/post-detail.component.spec.ts
+++ src/app/components/post-detail/post-detail.component.spec.ts
@@ -3,6 +3,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PostDetailComponent } from './post-detail.component';
 import { PostService } from 'src/app/services/post/post.service';
 import { ActivatedRoute } from '@angular/router';
+import { Post } from 'src/app/interfacs/post';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('PostDetailComponent', () => {
   let component: PostDetailComponent;
@@ -16,7 +21,7 @@ describe('PostDetailComponent', () => {
       'getPost',
       'updatePost',
     ]);
-    
+
     mockLocationSpy = jasmine.createSpyObj(['back']);
 
     mockActivatedRouteSpy = {
@@ -45,7 +50,8 @@ describe('PostDetailComponent', () => {
           useValue: mockPostService        
       }
       ],
-      imports: [],
+      schemas:[NO_ERRORS_SCHEMA],
+      imports: [FormsModule],
     });
 
     fixture = TestBed.createComponent(PostDetailComponent);
@@ -55,5 +61,19 @@ describe('PostDetailComponent', () => {
     mockPostService= TestBed.inject(PostService) as jasmine.SpyObj<PostService>;
   });
 
-  it('should create', () => {});
+  it('should render the post title in h2 template', () => {
+    mockPostService.getPost.and.returnValue(of({
+      id: 3,
+      title: 'title',
+      body: 'body',
+      userId:3
+    } as Post))
+    fixture.detectChanges();
+
+    // const element = fixture.debugElement.query(By.css('h2'))
+    //   .nativeElement as HTMLElement;
+    const element = fixture.nativeElement.querySelector(('h2')) as HTMLElement;
+    
+    expect(element.textContent).toBe(fixture.componentInstance.post.title);
+  });
 });
